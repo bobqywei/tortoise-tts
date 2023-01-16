0730c838f867489d10a09e8a57a55c8504311992
Bob Wei <bobqywei@gmail.com>
2023-01-16 04:31:25 -0500

    pipeline and data changes

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,94 +1,134 @@
-import boto3
 import os
+import boto3
+import uuid
+import argparse
 
+import aws_config
 
-def get_audio_files():
-    
-    rootdir = os.path.dirname(os.path.realpath(__file__))
+parser = argparse.ArgumentParser()
+parser.add_argument('--audio', type=str, help='Audio data dir.', default=None)
 
-    counter = 0
-    for subdir, dirs, files in os.walk(rootdir):
+def get_audio_files(audio_dir):
+    audio_files = []
+    categories = []
+    for root, dirs, files in os.walk(audio_dir):
         for file in files:
-            print(os.path.join(subdir, file))
-            counter += 1
-    print(counter)
 
-get_audio_files()
-
-# def upload_s3(bucket_name):
-
-#     # Set up a client to the S3 service
-#     s3 = boto3.client('s3')
-
-#     # Set the name of the S3 bucket
-#     bucket_name = 'raw-audio'
-
-#     # List of files to upload
-#     files = [
-#         {
-#             'file_name': 'gatsby.jpg',
-#             'file_path': '/path/to/gatsby.jpg',
-#             'file_type': 'image/jpeg'
-#         },
-#         {
-#             'file_name': 'mockingbird.jpg',
-#             'file_path': '/path/to/mockingbird.jpg',
-#             'file_type': 'image/jpeg'
-#         },
-#         {
-#             'file_name': 'gatsby.mp3',
-#             'file_path': '/path/to/gatsby.mp3',
-#             'file_type': 'audio/mpeg'
-#         },
-#         {
-#             'file_name': 'mockingbird.mp3',
-#             'file_path': '/path/to/mockingbird.mp3',
-#             'file_type': 'audio/mpeg'
-#         },
-#     ]
-
-#     # Iterate through the files and upload them to S3
-#     for file in files:
-#         s3.upload_file(file['file_path'], bucket_name, file['file_name'], ExtraArgs={'ContentType': file['file_type']})
+            if 'combined' not in file:
+                continue
+            filename, category = root.split('---')[0].split('/')[-1], root.split('---')[1]
+            author = root.split('/')[-3]
+            voice = root.split('/')[-2]
+            audio_files.append({
+                'file_name': 'public/audio/' + author + '/' + voice + '/' + filename + '.wav',
+                'file_path': os.path.join(root, file),
+                'file_type': 'audio/xwav'
+            })
+            categories.append(category.split('.wav')[0])
+    
+    return audio_files, categories
 
 
-# def upload_dynamodb():
+def upload_s3(bucket_name, files):
 
-#     # Set up a client to the DynamoDB service
-#     dynamodb = boto3.client('dynamodb')
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=aws_config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=aws_config.AWS_SECRET_ACCESS_KEY,
+    )
 
-#     # Create a list of essay items to be added to the table
-#     essay_list = [
-#         {
-#             'id': {'S': 'essay1'},
-#             'name': {'S': 'The Great Gatsby'},
-#             'imageUri': {'S': 'https://example.com/gatsby.jpg'},
-#             'audioUri': {'S': 'https://example.com/gatsby.mp3'},
-#             'essayCategoryId': {'S': 'category1'},
-#             'authorId': {'S': 'author1'}
-#         },
-#         {
-#             'id': {'S': 'essay2'},
-#             'name': {'S': 'To Kill a Mockingbird'},
-#             'imageUri': {'S': 'https://example.com/mockingbird.jpg'},
-#             'audioUri': {'S': 'https://example.com/mockingbird.mp3'},
-#             'essayCategoryId': {'S': 'category2'},
-#             'authorId': {'S': 'author2'}
-#         }
-#     ]
+    file_urls = []
+    for file in files:
+        s3.upload_file(file['file_path'], bucket_name, file['file_name'])
+        file_url = '%s/%s/%s' % (s3.meta.endpoint_url, bucket_name, file['file_name'])
+        file_urls.append(file_url)
 
-#     # Use the DynamoDB BatchWriteItem API to add the essays to the table
-#     respons            'Item': essay
-#                     }
-#             e = dynamodb.batch_write_item(
-#         RequestItems={
-#             'EssayTable': [
-#                 {
-#                     'PutRequest': {
-#             } for essay in essay_list
-#             ]
-#         }
-#     )
+    return file_urls
 
-#     # Print the response to check for any errors
-#     print(response)
+
+def upload_dynamo_db(essays_data, category_data):
+
+    dynamodb_resource = boto3.resource(
+        'dynamodb',
+        region_name='us-west-1',
+        aws_access_key_id=aws_config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=aws_config.AWS_SECRET_ACCESS_KEY,)
+
+    table = dynamodb_resource.Table(aws_config.AUTHOR_TABLE_NAME)
+
+    for essay in essays_data:
+        author_name = essay["author_name"]
+        response = table.query(
+            IndexName="byAuthorName",
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('name').eq(author_name),
+        )
+        if "Items" not in response or len(response["Items"]) == 0:
+            author_id = str(uuid.uuid4())
+            print('Putting new author: ', author_name)
+            table.put_item(
+                Item={
+                    "id": author_id,
+                    "name": essay["author_name"],
+                    "imageUri": essay["authorImageUri"]
+                }
+            )
+            essay['authorId'] = author_id
+        else:
+            essay['authorId'] = response["Items"][0]["id"]
+
+        essay.pop('author_name')
+
+    essay_table = dynamodb_resource.Table(aws_config.ESSAY_TABLE_NAME)
+    for essay in essays_data:
+        response = essay_table.query(
+            IndexName="byEssayNameAndAuthor",
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('name').eq(essay['name']) & (
+                boto3.dynamodb.conditions.Key('authorId').eq(essay['authorId'])),
+        )
+        if "Items" not in response or len(response["Items"]) == 0:
+            print('Putting new essay: ', essay['name'])
+            essay_table.put_item(Item=essay)
+    
+    category_table = dynamodb_resource.Table(aws_config.ESSAY_CATEGORY_TABLE_NAME)
+    for category in category_data:
+        response = category_table.query(
+            IndexName="byEssayCategoryName",
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('name').eq(category['name']))
+        if "Items" not in response or len(response["Items"]) == 0:
+            print('Putting new category: ', category['name'])
+            category_table.put_item(Item=category)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    audio_dir = args.audio
+
+    files, categories = get_audio_files(audio_dir=audio_dir)
+    file_urls = upload_s3(aws_config.S3_BUCKET_NAME, files)
+
+    # with open('file_urls.txt', 'w') as f:
+    #     for url in file_urls:
+    #         f.write(url + '\n')
+
+    # with open('file_urls.txt', 'r') as f:
+    #     file_urls = f.readlines()
+
+    essays_data = [
+        {
+            "id": str(uuid.uuid4()),
+            "name": file_urls[x].split('/')[-1].split('.wav')[0],
+            "imageUri": "s3://spotifye549ed42da7f4fada97d00a50269aa43154513-default/public/author_images/paul_graham.png",
+            "audioUri": file_urls[x],
+            "essayCategoryId": categories[x],
+            "authorImageUri": "s3://spotifye549ed42da7f4fada97d00a50269aa43154513-default/public/author_images/paul_graham.png",
+            # author name is temporarily needed to get author id
+            "author_name": file_urls[x].split('/')[-3]
+        } for x in range(len(files))
+    ]
+    categories_data = [
+        {
+            "id": str(uuid.uuid4()),
+            "name": category
+        } for category in set(categories)
+    ]
+    upload_dynamo_db(essays_data, categories_data)
--- tortoise/read.py
+++ tortoise/read.py
@@ -19,24 +19,23 @@ if __name__ == '__main__':
     parser.add_argument('--textdir', type=str, help='A dir containing the texts to read.', default=None)
     parser.add_argument('--voice', type=str, help='Selects the voice to use for generation. See options in voices/ directory (and add your own!) '
                                                  'Use the & character to join two voices together. Use a comma to perform inference on multiple voices.', default='pat')
-    parser.add_argument('--output_path', type=str, help='Where to store outputs.', default='results/longform/')
+    parser.add_argument('--outdir', type=str, help='Where to store outputs.', default='results/')
     parser.add_argument('--preset', type=str, help='Which voice preset to use.', default='standard')
     parser.add_argument('--regenerate', type=str, help='Comma-separated list of clip numbers to re-generate, or nothing.', default=None)
     parser.add_argument('--candidates', type=int, help='How many output candidates to produce per-voice. Only the first candidate is actually used in the final product, the others can be used manually.', default=1)
-    parser.add_argument('--retries', type=int, help='How many output candidates to produce per-voice. Only the first candidate is actually used in the final product, the others can be used manually.', default=0)
     parser.add_argument('--batch_size', type=int, help='Batch size to use.', default=16)
     parser.add_argument('--model_dir', type=str, help='Where to find pretrained model checkpoints. Tortoise automatically downloads these to .models, so this'
                                                       'should only be specified if you have custom checkpoints.', default=MODELS_DIR)
     parser.add_argument('--seed', type=int, help='Random seed which can be used to reproduce results.', default=None)
     parser.add_argument('--produce_debug_state', type=bool, help='Whether or not to produce debug_state.pth, which can aid in reproducing problems. Defaults to true.', default=True)
+    parser.add_argument('--fix', type=bool, help='Enable failure fixing mode.', default=False)
 
     args = parser.parse_args()
-    tts = TextToSpeech(models_dir=args.model_dir, autoregressive_batch_size=args.batch_size)
-    if args.retries > 0:
-        stt = whisper.load_model("large-v2")
-
-    outpath = args.output_path
     regenerate = args.regenerate
+    tts = TextToSpeech(models_dir=args.model_dir, autoregressive_batch_size=args.batch_size)
+    stt = whisper.load_model("large-v2")
+    seed = int(time()) if args.seed is None else args.seed
+
     if args.textdir is not None:
         textfiles = glob.glob(os.path.join(args.textdir, '*.txt'))
     else:
@@ -47,6 +46,7 @@ if __name__ == '__main__':
 
     for textfile in textfiles:
         filename = textfile.split('/')[-1].split('.')[0]
+
         # Process text
         with open(textfile, 'r', encoding='utf-8') as f:
             text = ' '.join([l for l in f.readlines()])
@@ -57,15 +57,22 @@ if __name__ == '__main__':
         else:
             texts = split_and_recombine_text(text)
 
-        seed = int(time()) if args.seed is None else args.seed
         for selected_voice in selected_voices:
-            voice_outpath = os.path.join(outpath, selected_voice, filename)
-            if regenerate is None and os.path.isdir(voice_outpath):
-                continue
-            os.makedirs(voice_outpath, exist_ok=True)
+            outdir = os.path.join(args.outdir, selected_voice, filename)
 
-            if args.retries > 0:
-                failed = {}
+            if args.fix and os.path.isdir(outdir):
+                fail_path = os.path.join(outdir, 'fails')
+                if os.path.exists(fail_path):
+                    with open(fail_path, 'r') as f:
+                        regenerate = [int(e) for e in f.readline().split(',')]
+                
+            # Skip if already generated and not regenerating
+            if regenerate is None and os.path.isdir(outdir):
+                continue
+            os.makedirs(outdir, exist_ok=True)
+
+            # Track if clips failed speech-to-text test
+            failed = {}
 
             if '&' in selected_voice:
                 voice_sel = selected_voice.split('&')
@@ -73,73 +80,55 @@ if __name__ == '__main__':
                 voice_sel = [selected_voice]
 
             voice_samples, conditioning_latents = load_voices(voice_sel)
+
             all_parts = []
             for j, text in enumerate(texts):
-                with open(os.path.join(voice_outpath, f'{j}.txt'), 'w') as f:
+
+                # Write text clip to file to match audio clips
+                with open(os.path.join(outdir, f'{j}.txt'), 'w') as f:
                     f.write(text)
+
                 print(f'\nGenerating clip for {filename} {j}/{len(texts)}: {text}')
+                # Directly load audio if already generated and we are not regenerating this clip
                 if regenerate is not None and j not in regenerate:
-                    all_parts.append(load_audio(os.path.join(voice_outpath, f'{j}.wav'), 24000))
+                    all_parts.append(load_audio(os.path.join(outdir, f'{j}.wav'), 24000))
                     continue
 
+                wav_path = os.path.join(outdir, f'{j}.wav')
                 if args.candidates == 1:
-                    wav_path = os.path.join(voice_outpath, f'{j}.wav')
-
-                    if args.retries == 0:
-                        gen = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
-                                                preset=args.preset, k=args.candidates, use_deterministic_seed=seed)
-                        gen = gen.squeeze(0).cpu()
-                        torchaudio.save(wav_path, gen, 24000)
-
-                    else:
-                        passed = False
-                        for _ in range(args.retries):
-                            gen = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
-                                                    preset=args.preset, k=args.candidates, use_deterministic_seed=seed)
-                            gen = gen.squeeze(0).cpu()
-                            torchaudio.save(wav_path, gen, 24000)
-                            stt_text = stt.transcribe(wav_path)['text'].strip()
-                            gt_tokens = [t for t in text.split() if bool(re.search(r'[a-zA-Z0-9]', t))]
-                            stt_tokens = [t for t in stt_text.split() if bool(re.search(r'[a-zA-Z0-9]', t))]
-                            if len(stt_tokens) - len(gt_tokens) <= 1 or sum([len(t) for t in stt_tokens]) - sum([len(t) for t in gt_tokens]) <= 6:
-                                passed = True
-                                break
-                        if not passed:
-                            failed[str(j)] = (text, stt_text)
+                    gen = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
+                                            preset=args.preset, k=args.candidates, use_deterministic_seed=seed)
+                    gen = gen.squeeze(0).cpu()
+                    torchaudio.save(wav_path, gen, 24000)
 
                 else:
                     gen = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
                                             preset=args.preset, k=args.candidates, use_deterministic_seed=seed)
-                    candidate_dir = os.path.join(voice_outpath, str(j))
-                    os.makedirs(candidate_dir, exist_ok=True)
+                    passed = False
                     for k, g in enumerate(gen):
-                        torchaudio.save(os.path.join(candidate_dir, f'{k}.wav'), g.squeeze(0).cpu(), 24000)
-                    gen = gen[0].squeeze(0).cpu()
+                        gen = g.squeeze(0).cpu()
+                        torchaudio.save(wav_path, gen, 24000)
+                        stt_text = stt.transcribe(wav_path)['text'].strip()
+                        gt_tokens = [t for t in text.split() if bool(re.search(r'[a-zA-Z0-9]', t))]
+                        stt_tokens = [t for t in stt_text.split() if bool(re.search(r'[a-zA-Z0-9]', t))]
+                        if len(stt_tokens) - len(gt_tokens) <= 1 or sum([len(t) for t in stt_tokens]) - sum([len(t) for t in gt_tokens]) <= 6:
+                            passed = True
+                            break
+                    if not passed:
+                        failed[str(j)] = (text, stt_text)
 
                 all_parts.append(gen)
 
-            if args.retries > 0 and failed:
-                with open(os.path.join(voice_outpath, 'fails'), 'w') as f:
+            with open(os.path.join(outdir, 'fails'), 'w') as f:
+                if len(failed) > 0:
                     f.write(','.join(failed.keys()) + '\n')
                     for i, (gt_text, stt_text) in failed.items():
                         f.write(f'{i}:\n{gt_text}\n{stt_text}\n')
 
-            if args.candidates == 1:
-                full_audio = torch.cat(all_parts, dim=-1)
-                torchaudio.save(os.path.join(voice_outpath, 'combined.wav'), full_audio, 24000)
+            full_audio = torch.cat(all_parts, dim=-1)
+            torchaudio.save(os.path.join(outdir, 'combined.wav'), full_audio, 24000)
 
             if args.produce_debug_state:
                 os.makedirs('debug_states', exist_ok=True)
                 dbg_state = (seed, texts, voice_samples, conditioning_latents)
                 torch.save(dbg_state, f'debug_states/read_debug_{selected_voice}.pth')
-
-            # Combine each candidate's audio clips.
-            if args.candidates > 1:
-                audio_clips = []
-                for candidate in range(args.candidates):
-                    for line in range(len(texts)):
-                        wav_file = os.path.join(voice_outpath, str(line), f"{candidate}.wav")
-                        audio_clips.append(load_audio(wav_file, 24000))
-                    audio_clips = torch.cat(audio_clips, dim=-1)
-                    torchaudio.save(os.path.join(voice_outpath, f"combined_{candidate:02d}.wav"), audio_clips, 24000)
-                    audio_clips = []
