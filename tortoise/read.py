64a629b7239ee2b51a8155710adc8c0a909eab37
Bob Wei <bobqywei@gmail.com>
2023-01-18 04:15:49 -0500

    update read script

--- tortoise/read.py
+++ tortoise/read.py
@@ -13,10 +13,10 @@ from utils.text import split_and_recombine_text
 from scripts.stt import check_texts_approx_match
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--textdir', type=str, help='A dir containing the texts to read.', default=None)
+parser.add_argument('--textdirs', type=str, help='A dir containing the texts to read. Multiple dirs can be specified in a comma separated list', default=None)
 parser.add_argument('--voice', type=str, help='Selects the voice to use for generation. See options in voices/ directory (and add your own!) '
                                                 'Use the & character to join two voices together. Use a comma to perform inference on multiple voices.', default='pat')
-parser.add_argument('--outdir', type=str, help='Where to store outputs.', default='results/')
+parser.add_argument('--outdirs', type=str, help='Where to store outputs. Must match the number of textdirs.', default='results/')
 parser.add_argument('--candidates', type=int, help='How many output candidates to produce per-voice. Only the first candidate is actually used in the final product, the others can be used manually.', default=1)
 parser.add_argument('--fix', type=bool, help='Enable failure fixing mode.', default=False)
 parser.add_argument('--qa', type=bool, help='Enable QA with stt.', default=False)
@@ -42,101 +42,109 @@ if __name__ == '__main__':
     if use_stt:
         stt = whisper.load_model("large-v2")
 
-    if args.textdir is not None:
-        textfiles = glob.glob(os.path.join(args.textdir, '*.txt'))
+    if args.textdirs is not None:
+        textdirs = args.textdirs.split(',')
+        outdirs = args.outdirs.split(',')
+        textdir_files = []
+        assert len(textdirs) == len(outdirs), "Must have the same number of textdirs and outdirs"
+        for textdir in textdirs:
+            textdir_files.append(glob.glob(os.path.join(textdir, '*.txt')))
     else:
-        textfiles = [args.textfile]
+        textdir_files = [[args.textfile]]
+        outdirs = args.outdirs.split(',')
+        assert len(outdirs) == 1, "When using textfile, must have exactly one audio_dir"
         if regenerate is not None:
             regenerate = [int(e) for e in regenerate.split(',')]
 
-    for i, textfile in enumerate(textfiles):
-        filename = textfile.split('/')[-1].split('.')[0]
+    for k, (textfiles, outdir) in enumerate(zip(textdir_files, outdirs)):
+        for i, textfile in enumerate(textfiles):
+            filename = textfile.split('/')[-1].split('.')[0]
 
-        # Process text
-        with open(textfile, 'r', encoding='utf-8') as f:
-            text = ' '.join([l for l in f.readlines()])
-        if '|' in text:
-            print("Found the '|' character in your text, which I will use as a cue for where to split it up. If this was not"
-                "your intent, please remove all '|' characters from the input.")
-            texts = text.split('|')
-        else:
-            texts = split_and_recombine_text(text)
-
-        for selected_voice in selected_voices:
-            outdir = os.path.join(args.outdir, selected_voice, filename)
-
-            # If we're in fix mode, check which clips failed and regenerate only those
-            if args.fix and os.path.isdir(outdir):
-                fail_path = os.path.join(outdir, 'fails')
-                if os.path.exists(fail_path):
-                    with open(fail_path, 'r') as f:
-                        line = f.readline().strip()
-                        if line:
-                            regenerate = [int(e) for e in line.split(',')]
-            elif not os.path.isdir(outdir):
-                os.makedirs(outdir, exist_ok=True)
-
-            # Get voice samples and voice conditioning latents
-            if '&' in selected_voice:
-                voice_sel = selected_voice.split('&')
+            # Process text
+            with open(textfile, 'r', encoding='utf-8') as f:
+                text = ' '.join([l for l in f.readlines()])
+            if '|' in text:
+                print("Found the '|' character in your text, which I will use as a cue for where to split it up. If this was not"
+                    "your intent, please remove all '|' characters from the input.")
+                texts = text.split('|')
             else:
-                voice_sel = [selected_voice]
-            voice_samples, conditioning_latents = load_voices(voice_sel)
+                texts = split_and_recombine_text(text)
 
-            failed = {}
-            all_parts = []
-            for j, text in enumerate(texts):
-                print(f'\n{i}/{len(textfiles)}: {filename} {j}/{len(texts)}: {text} --- {selected_voice}')
+            for selected_voice in selected_voices:
+                audio_dir = os.path.join(outdir, selected_voice, filename)
 
-                # Write text clip to file to match audio clips
-                with open(os.path.join(outdir, f'{j}.txt'), 'w') as f:
-                    f.write(text)
+                # If we're in fix mode, check which clips failed and regenerate only those
+                if args.fix and os.path.isdir(audio_dir):
+                    fail_path = os.path.join(audio_dir, 'fails')
+                    if os.path.exists(fail_path):
+                        with open(fail_path, 'r') as f:
+                            line = f.readline().strip()
+                            if line:
+                                regenerate = [int(e) for e in line.split(',')]
+                elif not os.path.isdir(audio_dir):
+                    os.makedirs(audio_dir, exist_ok=True)
 
-                wav_path = os.path.join(outdir, f'{j}.wav')
-                # Skip if we are not regenerating this clip and audio exists
-                if (not regenerate or j not in regenerate) and os.path.exists(wav_path):
-                    all_parts.append(load_audio(wav_path, 24000))
-                    continue
+                # Get voice samples and voice conditioning latents
+                if '&' in selected_voice:
+                    voice_sel = selected_voice.split('&')
+                else:
+                    voice_sel = [selected_voice]
+                voice_samples, conditioning_latents = load_voices(voice_sel)
 
-                generated = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
-                                                preset=args.preset, k=args.candidates, use_deterministic_seed=seed)
-                if args.candidates == 1:
-                    generated = [generated]
+                failed = {}
+                all_parts = []
+                for j, text in enumerate(texts):
+                    print(f'\n{k}/{len(outdirs)}: {outdir}\n{i}/{len(textfiles)}: {filename}\n{j}/{len(texts)}: {text}\n{selected_voice}')
 
-                passed = False
-                for k, g in enumerate(generated):
-                    gen = g.squeeze(0).cpu()
-                    torchaudio.save(wav_path, gen, 24000)
-                    if use_stt:
-                        result = stt.transcribe(wav_path)
-                        # Save the sentence timestamps
-                        with open(os.path.join(outdir, f'{j}.timestamps'), "w") as f:
-                            for segment in result['segments']:
-                                f.write(f"{segment['start']}-{segment['end']}: {segment['text']}\n")
-                        # QA with stt results
-                        if check_texts_approx_match(text, result['text'].strip()):
-                            passed = True
-                            break
+                    # Write text clip to file to match audio clips
+                    with open(os.path.join(audio_dir, f'{j}.txt'), 'w') as f:
+                        f.write(text)
 
-                all_parts.append(gen)
-                if not passed:
-                    failed[str(j)] = (text, result['text'].strip())
+                    wav_path = os.path.join(audio_dir, f'{j}.wav')
+                    # Skip if we are not regenerating this clip and audio exists
+                    if (not regenerate or j not in regenerate) and os.path.exists(wav_path):
+                        all_parts.append(load_audio(wav_path, 24000))
+                        continue
 
-            # Save the clip ids that failed speech-to-text test
-            if use_stt:
-                fail_path = os.path.join(outdir, 'fails')
-                if len(failed) > 0:
-                    with open(fail_path, 'w') as f:
-                        f.write(','.join(failed.keys()) + '\n')
-                        for i, (gt_text, stt_text) in failed.items():
-                            f.write(f'{i}:\n{gt_text}\n{stt_text}\n')
-                elif os.path.exists(fail_path):
-                    os.remove(fail_path)
+                    generated = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
+                                                    preset=args.preset, k=args.candidates, use_deterministic_seed=seed)
+                    if args.candidates == 1:
+                        generated = [generated]
 
-            full_audio = torch.cat(all_parts, dim=-1)
-            torchaudio.save(os.path.join(outdir, 'combined.wav'), full_audio, 24000)
+                    passed = False
+                    for k, g in enumerate(generated):
+                        gen = g.squeeze(0).cpu()
+                        torchaudio.save(wav_path, gen, 24000)
+                        if use_stt:
+                            result = stt.transcribe(wav_path)
+                            # Save the sentence timestamps
+                            with open(os.path.join(audio_dir, f'{j}.timestamps'), "w") as f:
+                                for segment in result['segments']:
+                                    f.write(f"{segment['start']}-{segment['end']}: {segment['text']}\n")
+                            # QA with stt results
+                            if check_texts_approx_match(text, result['text'].strip()):
+                                passed = True
+                                break
 
-            if args.produce_debug_state:
-                os.makedirs('debug_states', exist_ok=True)
-                dbg_state = (seed, texts, voice_samples, conditioning_latents)
-                torch.save(dbg_state, f'debug_states/read_debug_{selected_voice}.pth')
+                    all_parts.append(gen)
+                    if not passed:
+                        failed[str(j)] = (text, result['text'].strip())
+
+                # Save the clip ids that failed speech-to-text test
+                if use_stt:
+                    fail_path = os.path.join(audio_dir, 'fails')
+                    if len(failed) > 0:
+                        with open(fail_path, 'w') as f:
+                            f.write(','.join(failed.keys()) + '\n')
+                            for i, (gt_text, stt_text) in failed.items():
+                                f.write(f'{i}:\n{gt_text}\n{stt_text}\n')
+                    elif os.path.exists(fail_path):
+                        os.remove(fail_path)
+
+                full_audio = torch.cat(all_parts, dim=-1)
+                torchaudio.save(os.path.join(audio_dir, 'combined.wav'), full_audio, 24000)
+
+                if args.produce_debug_state:
+                    os.makedirs('debug_states', exist_ok=True)
+                    dbg_state = (seed, texts, voice_samples, conditioning_latents)
+                    torch.save(dbg_state, f'debug_states/read_debug_{selected_voice}.pth')
